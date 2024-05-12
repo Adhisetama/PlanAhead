efad6c4712abdb8867a80d79274a87bb3901ede8
jearsevan101 <juliusevanars@gmail.com>
2024-05-12 12:00:09 +0700

    slicing ui header, footer, and homepage

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+const Footer = () =>{
+return (
+        <footer className="bg-[#F9C222] text-white mt-5 border-t border-gray-100">
+        <div className="flex justify-center items-center sm:px-16 px-6 py-10">
+            <div className="text-center">
+            <div className="flex justify-center items-center mb-4">
+                <Image
+                src="/header-logo.png"
+                alt="Logo"
+                width={100}
+                height={50}
+                />
+            </div>
+            <p>
+                Here to help you to manage your plans!
+            </p>
+            <div className="border-t border-white flex my-4 mx-0"></div>
+            <p>
+                Â©2024 PlanAhead
+            </p>
+            </div>
+        </div>
+        </footer>
+    )
+}
+
+export default Footer;
