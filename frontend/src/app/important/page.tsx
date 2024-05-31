83602c108cd156d2509fcc1c83dc0d7abae21902
jearsevan101 <juliusevanars@gmail.com>
2024-05-31 23:26:58 +0700

    mapping repeatable task and completed task

---
+++ frontend/src/app/important/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+function Important() {
+  return (
+    <div>This is the Important page</div>
+  )
+}
+
+export default Important
--- frontend/src/app/repeatable/page.tsx
+++ frontend/src/app/repeatable/page.tsx
@@ -4,9 +4,9 @@ import Tasks from "../../components/Tasks";
 import { useGlobalState } from "../context/globalProvider";
 import styled from 'styled-components';
 
-export default function CompletedTasks() {
+export default function RepeatableTasks() {
   const {tasks} = useGlobalState();
-  const completedTasks = tasks.filter(task => task.isCompleted);
+  const repeatableTasks = tasks.filter(task => task.isRepeatable);
 
-  return <Tasks title="Completed Tasks" tasks={completedTasks} />;
+  return <Tasks title="Repeatable Tasks" tasks={repeatableTasks} />;
 }
